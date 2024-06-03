76983290e496269d82f9554f5f469a09ac614d03
nazo6 <git@nazo6.dev>
2024-06-03 18:39:25 +0900

    update

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/nazo6/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/nazo6/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define SPLIT_HAND_MATRIX_GRID_LOW_IS_LEFT
